a8caa7c8536ec209aa2b406c8d0e12b688db1d45
shepherrrd <shefkid2@gmail.com>
2023-12-15 19:40:26 +0100

    Trying dynamic route handling

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,32 @@
+mod router;
+
+use router::Router;
 use std::net::{TcpListener, TcpStream};
 use std::io::prelude::*;
-mod router; 
 
-fn handle_connection(mut stream: TcpStream) {
+fn handle_connection(mut stream: TcpStream, router: &Router) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
-    
-    let request = String::from_utf8_lossy(&buffer[..]);
-    let response = router::route_request(&request);
+
+    let request_line = String::from_utf8_lossy(&buffer[..]).lines().next().unwrap_or("");
+    let path = request_line.split_whitespace().nth(1).unwrap_or("");
+
+    let response = router.route(path);
 
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let mut router = Router::new();
+    router.add_route("/hello/v1".to_string(), "HTTP/1.1 200 OK\r\n\r\nHello, V1!".to_string());
 
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
+        handle_connection(stream, &router);
     }
+}
+fn sample(){
+    
 }
--- src/router.rs
+++ src/router.rs
@@ -1,28 +1,78 @@
+use serde_json::json;
+use std::collections::HashMap;
 
+pub struct Router {
+    routes: HashMap<String, String>,
+}
+
+impl Router {
+    pub fn new() -> Router {
+        Router {
+            routes: HashMap::new(),
+        }
+    }
+
+    pub fn add_route(&mut self, path: String, response: String) {
+        self.routes.insert(path, response);
+    }
+
+    pub fn route(&self, path: &str) -> String {
+        self.routes.get(path).cloned().unwrap_or_else(|| "HTTP/1.1 404 NOT FOUND\r\n\r\nRoute not found".to_string())
+    }
+}
 pub fn route_request(request: &str) -> String {
     let lines: Vec<&str> = request.lines().collect();
     let request_line = lines[0];
     let request_parts: Vec<&str> = request_line.split_whitespace().collect();
+    if request_parts.len() < 2 {
+        println!("Bad request: {}", request);
+        return "HTTP/1.1 400 BAD REQUEST\r\n\r\nBad Request".to_string();
+    }
     let method = request_parts[0];
     let path = request_parts[1];
 
     match method {
-        "GET" => handle_get(path),
-        "POST" => handle_post(path),
-        _ => "HTTP/1.1 405 METHOD NOT ALLOWED\r\n\r\nMethod Not Allowed".to_string(),
+        "GET" => return handle_get(path),
+        "POST" => return handle_post(path),
+        _ =>  return "HTTP/1.1 405 METHOD NOT ALLOWED\r\n\r\nMethod Not Allowed".to_string(),
     }
 }
 
 fn handle_get(path: &str) -> String {
+    let response;
     match path {
-        "/hello" => "HTTP/1.1 200 OK\r\n\r\nHello from GET!".to_string(),
-        _ => "HTTP/1.1 404 NOT FOUND\r\n\r\nPage not found".to_string(),
+        "/hello" => {
+            let message = json!({
+                "message": "Hello from Shepherd!"
+            });
+            response = format!("HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n\r\n{}", serde_json::to_string(&message).unwrap());
+        },
+        _ => {
+            let error = json!({
+                "error": "Page not found"
+            });
+            response = format!("HTTP/1.1 404 NOT FOUND\r\nContent-Type: application/json\r\n\r\n{}", serde_json::to_string(&error).unwrap());
+        },
     }
+    response
 }
 
 fn handle_post(path: &str) -> String {
+    let response;
     match path {
-        "/hello" => "HTTP/1.1 200 OK\r\n\r\nHello from POST!".to_string(),
-        _ => "HTTP/1.1 404 NOT FOUND\r\n\r\nPage not found".to_string(),
+        "/hello" => {
+            let message = json!({
+                "message": "Hello from POST!"
+            });
+            response = format!("HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n\r\n{}", serde_json::to_string(&message).unwrap());
+        },
+        _ => {
+            let error = json!({
+                "error": "Page not found"
+            });
+            response = format!("HTTP/1.1 404 NOT FOUND\r\nContent-Type: application/json\r\n\r\n{}", serde_json::to_string(&error).unwrap());
+        },
     }
+    response
 }
+
